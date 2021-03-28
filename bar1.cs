afcb06d9f5be96678276f39801b93ff9342a4ef9
Qiongyun Zhang <51444652+Todoorno@users.noreply.github.com>
2021-03-28 11:06:31 +0800

    add file

---
+++ bar1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+public class bar1 : MonoBehaviour
+{
+    private int flag = 0;
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow) )
+        {
+             
+            if (flag == 0)
+            {         
+                GetComponent<Transform>().RotateAround(Vector3.up, -120f);              
+                flag = 1;
+            }               
+        }
+        else flag = 0;
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            if (flag == 0)
+            {
+                GetComponent<Transform>().RotateAround(Vector3.down, -120f);
+                flag = 1;
+            }
+        }
+        else flag = 0;
+
+
+    }
+}
